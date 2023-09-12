92f9f074c1fd64f5bca3a0561604bf35d5b18579
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-12 17:00:54 +0900

    변수
    변수 타입, 선언, 할당, 나열, 초기화 등

---
+++ WorkSpace/02.Variable/src/pack01/variable/Ex02_Variable.java
@@ -0,0 +1,21 @@
+package pack01.variable;
+
+//ctrl + shift + (+,-) : 화면 클씨 크기 조정
+public class Ex02_Variable {
+public static void main(String[] args) {
+	//나열 : 변수를 여러개를 동시에 선언하는것.
+	//int x;
+	//int y;.... -> int x , y , z ;
+	int num1 , num2 , num3 ; // int타입의 변수 3개 동시 선언
+	//변수를 선언과 동시에 값을 주고 사용할 준비를 마침. => 초기화
+	int initNumber = 10; // 변수의 초기화식.
+	
+	int x=10 , y=20 , z=10 ; //나열을 이용한 초기화식
+	
+	
+	
+	
+	
+	
+}
+}
---
+++ WorkSpace/02.Variable/src/pack01/variable/Test01.java
@@ -0,0 +1,16 @@
+package pack01.variable;
+
+public class Test01 {
+public static void main(String[] args) {
+	int age;
+	int tall;
+	int length;
+	age = 10;
+	tall = 100;
+	length = 1000;
+	
+	System.out.println(age);
+	System.out.println(tall);
+	System.out.println(length);
+}
+}
