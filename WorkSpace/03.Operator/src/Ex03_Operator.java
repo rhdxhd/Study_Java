875d20e60a0f867da9892cce717a597524d14b19
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-15 14:37:09 +0900

    Update Ex03_Operator.java

--- WorkSpace/03.Operator/src/Ex03_Operator.java
+++ WorkSpace/03.Operator/src/Ex03_Operator.java
@@ -1,4 +1,31 @@
 
 public class Ex03_Operator {
+	public static void main(String[] args) {
+		// ++, --
+		//복습해도됨. 엄청 많이씀, 증감, 가감 (현실에서는 안씀)
+		int num1 = 1 , num2 = 1;
+		
+		num1++; // 변수+1
+		// num2++; -> ++num2;로 바꿔도 값이 똑같이 나옴? 
+		++num2;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		// ※ 선행 연산: 변수 앞에 증감식을 사용, 후행 연산: 변수 뒤에 증감식을 사용 ※//		
+		
+		System.out.println(num1++);
+		System.out.println(++num2);
+		
+		System.out.println(" 증감식 종료 후 num1: " + num1);
+		System.out.println(" 증감식 종료 후 num2: " + num2);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
