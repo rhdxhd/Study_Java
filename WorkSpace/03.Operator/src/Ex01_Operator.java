ae467e3018a43a75007802ede7cb7b3b29f57e48
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-15 14:30:28 +0900

    .
    .

---
+++ WorkSpace/03.Operator/src/Ex01_Operator.java
@@ -0,0 +1,21 @@
+
+public class Ex01_Operator {
+	public static void main(String[] args) {
+		// int형 변수num1과 num2에 각각 2와 3을 담고 초기화
+		int num1 = 2, num2 = 3;
+		//int num2 = 2;
+		//int num2 = 3;
+		// System.out.println(1+1);
+		// - , * , /
+		// ctrl + alt + 방향 위 아래 : 복사
+		System.out.println("num1 + num2 =" + ( num1 + num2)); //syso(값하나)
+		System.out.println("num1 - num2 =" + ( num1 - num2)); //syso(값하나)
+		System.out.println("num1 * num2 =" + ( num1 * num2)); //syso(값하나)
+		System.out.println("num1 / num2 =" + ( num1 / num2)); //syso(값하나)
+		System.out.println("num1 % num2 =" + ( num1 % num2)); //syso(값하나)
+		System.out.println("2로 나누고 나머지가 0이면 짝수" + (num1%2)); 
+		System.out.println("2로 나누고 나머지가 1이면 홀수" + (num1%2)); 
+		
+	
+}
+}
