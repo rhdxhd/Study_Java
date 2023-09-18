3944f3631508e51a1a136691c7e45fa8a3cbaa27
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-18 17:34:57 +0900

    Update Test02_IfElse.java

--- WorkSpace/04.ControlStatement/src/pack01/exif/Test02_IfElse.java
+++ WorkSpace/04.ControlStatement/src/pack01/exif/Test02_IfElse.java
@@ -1,5 +1,12 @@
 package pack01.exif;
 
 public class Test02_IfElse {
+		public static void main(String[] args) {
+		//int num = 10; 초기화
+		//해당 수를 짝수인지 홀수인지를 판단해서 각각 출력하는 프로그램을 만들기
+		//조건. if와 else를 사용할것
+			
+			int number = 10;
+		}
 
 }
