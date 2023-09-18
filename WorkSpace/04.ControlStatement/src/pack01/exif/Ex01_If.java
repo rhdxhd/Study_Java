acb253c92a7beae8987b958b7557dd4f4c271d0b
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-18 17:47:12 +0900

    제어문 1일차

--- WorkSpace/04.ControlStatement/src/pack01/exif/Ex01_If.java
+++ WorkSpace/04.ControlStatement/src/pack01/exif/Ex01_If.java
@@ -16,14 +16,14 @@ public class Ex01_If {
 		int score = 100;
 		if (score >= 90) { // <= 조건식에 score가 90점 이상이라면을 넣어보자
 			int if_var1 = 100;
-			System.out.println("90점 이상이여서 장학금 받음.");
+			System.out.println("90점 이상이여서 장학금 받음." + if_var1);
 			if (score == 100) {
 				int if_var2 = 100;
-				System.out.println("전교 1등입니다");
+				System.out.println("전교 1등입니다" + if_var1 + if_var2);
 			}
-			// System.out.println(if_var2) if지역 밖에서 사용 불가
+			// System.out.println(if_var2) if지역 밖에서 사용 불가 {}
 		}
-		// System.out.println(if_var1) if지역 밖에서 사용 불가
+		// System.out.println(if_var1) if지역 밖에서 사용 불가 {}
 		
 
 		// 만약에 점수가 100점이라면 "장학금을 받음"도 실행이되고,
