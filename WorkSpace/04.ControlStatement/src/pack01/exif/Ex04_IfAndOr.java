167125951b392b283dec9d29ac9e5b975810ecb5
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-09-19 15:19:19 +0900

    .

---
+++ WorkSpace/04.ControlStatement/src/pack01/exif/Ex04_IfAndOr.java
@@ -0,0 +1,5 @@
+package pack01.exif;
+
+public class Ex04_IfAndOr {
+
+}
--- WorkSpace/04.ControlStatement/src/pack01/exif/Test03_IF.java
+++ WorkSpace/04.ControlStatement/src/pack01/exif/Test03_IF.java
@@ -2,21 +2,11 @@ package pack01.exif;
 
 public class Test03_IF {
 	public static void main(String[] args) {
+		//char를 이용한 혈액형 분별 프로그램
 		
-	
-	
-	
-			//Test03_IF <- char타입을 이용하여
-			//char타입에 들어있는 값이 혈액형과 같은지 비교하는 if-else if-else문 만들기.
-			//혈액형과 같지 않다면 잘못된 입력! <- 출력
-			
-			char me = 'O';
-			if (me == O ) {
-				System.out.println("맞음");
-			} else if (me == B ) {
-				System.out.println("잘못된 입력");
-			}
-			
+		char booldType = 'A'; // <= html, android등의 다른 플랫폰에서 오는 데이터 ? 
+		
+		if(boo)
 	
 			
 			
