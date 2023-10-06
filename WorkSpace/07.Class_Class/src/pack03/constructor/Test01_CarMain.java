20f4f9645d0bacb66c3e548b53221fac6ca57aaf
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-10-06 17:13:02 +0900

    .

--- WorkSpace/07.Class_Class/src/pack03/constructor/Test01_CarMain.java
+++ WorkSpace/07.Class_Class/src/pack03/constructor/Test01_CarMain.java
@@ -1,14 +1,13 @@
 package pack03.constructor;
 
 public class Test01_CarMain {
+	//String company = null or "현대";
 	public static void main(String[] args) {
-		Test01_Car car = new Test01_Car();
-		car.company = "k";
-		car.color = "black";
-		car.model = "a";
-		car.name = "d";
-		
-		
+		Test01_Car car = new Test01_Car("현대" , "현대차");
+		//car.name = "현대차";
+		car.color = 999;
+		//car.company ="현대";
+		car.model ="SUV";
+		System.out.println(car.name + " " + car.model);
 	}
-
 }
