8586b6a987ec00cb7fa65bed4e5123ebb38aaf83
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-10-10 17:40:42 +0900

    .

--- WorkSpace/07.Class_2Method/src/pack01/method/ComputerMain.java
+++ WorkSpace/07.Class_2Method/src/pack01/method/ComputerMain.java
@@ -1,15 +1,14 @@
 package pack01.method;
 
 public class ComputerMain {
-	
 	public static void main(String[] args) {
-		
-	
-	
-	Computer pc = new Computer();
-	pc
-	if(pc.isPower= true) 
-	{ 
-
-}
+		Computer com = new Computer();
+		//if(com.isPower) {
+			com.plus(1, 2);
+			com.isPower = true;
+			com.plus(10, 20);
+		//}else {
+		//	System.out.println("전원 켜주세요.");
+		//}
+	}
 }
