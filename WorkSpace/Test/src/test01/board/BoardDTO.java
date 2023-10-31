458ae1fa86d1837baaafa5e8da4ebe7de876f064
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-10-31 16:27:00 +0900

    .

---
+++ WorkSpace/Test/src/test01/board/BoardDTO.java
@@ -0,0 +1,52 @@
+package test01.board;
+
+public class BoardDTO {
+	private String title, content, writer;
+	private int number;
+	
+	
+	
+	
+//	
+//	public BoardDTO(String title, String content, String writer, int number) {
+//		super();
+//		this.title = title;
+//		this.content = content;
+//		this.writer = writer;
+//		this.number = number;
+//	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+}
