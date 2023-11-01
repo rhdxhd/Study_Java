f5fade43baa65183c417d56d84eccc40000e32ed
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-11-01 17:53:28 +0900

    .

--- WorkSpace/16.ExternalAPI/src/pack00/collection/Main.java
+++ WorkSpace/16.ExternalAPI/src/pack00/collection/Main.java
@@ -1,5 +1,11 @@
 package pack00.collection;
 
-public class Main {
+import java.util.Scanner;
 
+import pack01.list.DepartmentDTO;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
 }
