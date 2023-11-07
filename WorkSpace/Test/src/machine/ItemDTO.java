68fca0fa10fbd06f391a5cc54ecdad1a44b956a3
rhdxhd <144658731+rhdxhd@users.noreply.github.com>
2023-11-07 16:56:57 +0900

    .

---
+++ WorkSpace/Test/src/machine/ItemDTO.java
@@ -0,0 +1,43 @@
+package machine;
+
+public class ItemDTO {
+	private String name, type, info;
+	private int price, stock, idx;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+}
---
+++ WorkSpace/Test/src/machine/Main.java
@@ -0,0 +1,8 @@
+package machine;
+
+public class Main {
+	public static void main(String[] args) {
+		DAO dao = new DAO();
+		dao.loginMenu();
+	}
+}
